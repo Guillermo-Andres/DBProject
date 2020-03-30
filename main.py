526764e46981d41dbe6a4c593a97aac1bf96cea1
guillermo-andres <guillermo.betancourt@upr.edu>
2020-03-30 19:05:16 -0400

    Initial Main empty dummy routes

--- main.py
+++ main.py
@@ -7,23 +7,70 @@ from handler.heavyequipment import HeavyEquipmentHandler
 from handler.ice import IceHandler
 from handler.powertools import PowerToolsHandler
 from handler.fuel import FuelHandler
-
-
-
-
-
 from flask_cors import CORS, cross_origin
+
+
 
 # Activate
 app = Flask(__name__)
 # Apply CORS to this app
 CORS(app)
 
-@app.route('/')
-def greeting():
-    return 'Hello, this is the almacenespr App!'
 
-@app.route('/almacenespr/resource/food', methods=['GET', 'PUT'])
+#4. Add a request for a given resource   4.)almacenespr/user<id>/orders   4.)POST de ORDER
+#5.)almacenespr/supplier<id>/newresource
+#6. Reserve or purchase a resource. Free resources are reserved. Otherwise, they are purchased. 6.)almacenespr/user<id>/orders (aqui mismo el front end se encarga de split entre reserved y eso) 
+#7.) almacenespr/requested/  7.) GET de order    7.) almacenespr/requested/
+#8.) almacenespr/available/  8.) GET de resource 8.) almacenespr/available/
+# 9. See detail of resources, including location on a Google Map    9.)almacenespr/resource<type>/resource<id> (todos los attributos de a given reource) 9.)GET de resource
+# 10. Keyword search resources being requested, with sorting by resource name             10.) almacenespr/requested/resource<type>/search<keyword>            10.)GET  de order
+# 11. Keyword search resources available, with sorting by resource name               11.)almacenespr/available/resource<type> /search<keyword>                11.) get resource
+# 12.)almacenespr/dashboard/statistics/daily/<type>
+# 13.)almacenespr/dashboard/statistics/weekly/<type>
+# 14.)almacenespr/dashboard/statistics/region<type>
+
+@app.route('/almacenespr/user/<int:user_id>/orders', methods = ['POST'])
+def orderResources(user_id):
+    #orders specify if we are requesting, reserving or purchasing depending on its status
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/supplier/<int:sid>/newresource', methods = ['POST'])
+def newResource(sid):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/requested', methods = ['GET'])
+def viewRequested():
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/available', methods = ['GET'])
+def viewAvailable():
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/<string:resource_type>/', methods = ['GET'])
+def getAllResources(resource_type):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/requested/<string:resource_type>/<string:search_keyword>', methods = ['GET'])
+def searchRequested(resource_type,search_keyword):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/available/<string:resource_type>/<string:search_keyword>', methods = ['GET'])
+def searchAvailable(resource_type,search_keyword):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/dashboard/statistics/daily/<int:type>', methods = ['GET'])
+def dailyStatistics(type):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/dashboard/statistics/weekly/<int:type>', methods = ['GET'])
+def weeklyStatistics(type):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/dashboard/statistics/region/<int:type>', methods = ['GET'])
+def regionStatistics(type):
+    return FoodHandler().getAllFood()
+
+@app.route('/almacenespr/food', methods=['GET', 'PUT'])
 def searchFood():
     if request.method == 'POST':
 
@@ -36,7 +83,7 @@ def searchFood():
             return PartHandler().searchParts(request.args)
 
 
-@app.route('/almacenespr/resource/medication', methods=['GET', 'PUT'])
+@app.route('/almacenespr/medication', methods=['GET', 'PUT'])
 def searchMedication():
     if request.method == 'POST':
 
@@ -48,7 +95,7 @@ def searchMedication():
         else:
             return PartHandler().searchParts(request.args)
 
-@app.route('/almacenespr/resource/batteries', methods=['GET', 'PUT'])
+@app.route('/almacenespr/batteries', methods=['GET', 'PUT'])
 def searchBatteries():
     if request.method == 'POST':
 
@@ -60,7 +107,7 @@ def searchBatteries():
         else:
             return PartHandler().searchParts(request.args)
 
-@app.route('/almacenespr/resource/clothes', methods=['GET', 'PUT'])
+@app.route('/almacenespr/clothes', methods=['GET', 'PUT'])
 def searchClothing():
     if request.method == 'POST':
 
@@ -72,7 +119,7 @@ def searchClothing():
         else:
             return PartHandler().searchParts(request.args)
 
-@app.route('/almacenespr/resource/heavyequipment', methods=['GET', 'PUT'])
+@app.route('/almacenespr/heavyequipment', methods=['GET', 'PUT'])
 def searchHeavyEquipment():
     if request.method == 'POST':
 
@@ -84,7 +131,7 @@ def searchHeavyEquipment():
         else:
             return PartHandler().searchParts(request.args)
 
-@app.route('/almacenespr/resource/ice', methods=['GET', 'PUT'])
+@app.route('/almacenespr/ice', methods=['GET', 'PUT'])
 def searchIce():
     if request.method == 'POST':
 
@@ -96,7 +143,7 @@ def searchIce():
         else:
             return PartHandler().searchParts(request.args)
 
-@app.route('/almacenespr/resource/tools', methods=['GET', 'PUT'])
+@app.route('/almacenespr/tools', methods=['GET', 'PUT'])
 def searchTools():
     if request.method == 'POST':
 
@@ -108,7 +155,7 @@ def searchTools():
         else:
             return PartHandler().searchParts(request.args)
 
-@app.route('/almacenespr/resource/fuel', methods=['GET', 'PUT'])
+@app.route('/almacenespr/fuel', methods=['GET', 'PUT'])
 def searchFuel():
     if request.method == 'POST':
 
