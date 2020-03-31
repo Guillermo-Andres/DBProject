31ba6b40bdf52857e4ccb974af6b2bfebd13a2e6
fabiola-badillo <fabiola.badillo@upr.edu>
2020-03-30 18:17:09 -0700

    handler for medication

--- handler/medication.py
+++ handler/medication.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from dao.medication import MedicationDAO
 
+    # Medication ATTTRIBUTES medication_id, name,ingredient,type,exp,price,location,quantity
 
 class MedicationHandler:
     def build_Medication_dict(self, row):
@@ -9,8 +10,10 @@ class MedicationHandler:
         result['name'] = row[1]
         result['ingredients'] = row[2]
         result['type'] = row[3]
-
-
+        result['expiration_date'] = row[4]
+        result['price'] = row[5]
+        result['location'] = row[6]
+        result['quantity'] = row[7]
         return result
 
 
@@ -23,3 +26,29 @@ class MedicationHandler:
             result = self.build_Medication_dict(row)
             result_list.append(result)
         return jsonify(Medication=result_list)
+
+    def getMedicationById(self, id):
+        return self.getAllMedication()
+
+    def getMedicationByName(self, name):
+        return self.getAllMedication()
+
+    def getMedicationByIngredients(self, ingredients):
+        return self.getAllMedication()
+
+    def getMedicationByType(self, type):
+        return self.getAllMedication()
+
+    def getMedicationByExpirationDate(self, expiration_date):
+        return self.getAllMedication()
+
+    def getMedicationByPrice(self, price):
+        return self.getAllMedication()
+
+    def getMedicationByLocation(self, location):
+        return self.getAllMedication()
+
+    def getMedicationByQuantity(self, quantity):
+        return self.getAllMedication()
+
+
