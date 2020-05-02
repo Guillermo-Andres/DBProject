e6cbaf7694d817bbe3f304c6f862caa68d9028d2
guillermo-andres <guillermo.betancourt@upr.edu>
2020-05-02 19:49:30 -0400

    Merging

--- dao/fuel.py
+++ dao/fuel.py
@@ -2,34 +2,49 @@ from config.dbconfig import pg_config
 import psycopg2
 class FuelDAO:
     #Fuel ATTTRIBUTES fid, type,unit_size,description,price,location,quantity
-    # def __init__(self):
-    #
-    #     connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
-    #                                                         pg_config['user'],
-    #                                                         pg_config['passwd'])
-    #     self.conn = psycopg2._connect(connection_url)
+    def __init__(self):
+
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+                                                            pg_config['user'],
+                                                            pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
 
     def getAllFuel(self):
-
-        result = [[1,'Propane','12 Oz' , '1 Propane gas Tank',0,'Maunabo',12],[2,'Gas','24 Galons', '24 Gallons of liquid fuel',6.99,'Carolina',24],[3,'Disel','5 Galons','5 Galons of Disel',56.00,'Mayaguez',15]]
+        cursor = self.conn.cursor()
+        query = "select * from fuel;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getFuelById(self, id):
-        result = [[1,'canned','no','beans','12oz','red beans','12/25/2022',0,'SanJuan',3],[2,'fruit','yes','organic bannanas','5 lb','bannana','12/25/2022', 4.99,'Ponce',6],[3,'meat','yes','codero flesh','8 Oz','Corderito','12/25/2022',0,'Aguadilla',1]]
+        cursor = self.conn.cursor()
+        query = "select * from fuel where fuel_id = %s;"
+        cursor.execute(query, (id,))
+        result = cursor.fetchone()
         return result
 
     def getFuelByType(self, type):
-         result = [[1,'canned','no','beans','12oz','red beans','12/25/2022',0,'SanJuan',3],[2,'fruit','yes','organic bannanas','5 lb','bannana','12/25/2022', 4.99,'Ponce',6],[3,'meat','yes','codero flesh','8 Oz','Corderito','12/25/2022',0,'Aguadilla',1]]
-         return result
+        cursor = self.conn.cursor()
+        query = "select * from fuel where fuel_type = %s;"
+        cursor.execute(query, (type,))
+        result = cursor.fetchone()
+        return result
 
     def getFuelBySize(self, size):
-        result = [[1,'canned','no','beans','12oz','red beans','12/25/2022',0,'SanJuan',3],[2,'fruit','yes','organic bannanas','5 lb','bannana','12/25/2022', 4.99,'Ponce',6],[3,'meat','yes','codero flesh','8 Oz','Corderito','12/25/2022',0,'Aguadilla',1]]
+        cursor = self.conn.cursor()
+        query = "select * from fuel where size = %s;"
+        cursor.execute(query, (pid,))
+        result = cursor.fetchone()
         return result
 
     def getFuelByPrice(self, price):
-        result = [[1,'canned','no','beans','12oz','red beans','12/25/2022',0,'SanJuan',3],[2,'fruit','yes','organic bannanas','5 lb','bannana','12/25/2022', 4.99,'Ponce',6],[3,'meat','yes','codero flesh','8 Oz','Corderito','12/25/2022',0,'Aguadilla',1]]
+        cursor = self.conn.cursor()
+        query = "select * from fuel where price = %s;"
+        cursor.execute(query, (pid,))
+        result = cursor.fetchone()
         return result
-
     def getFuelByLocation(self, location):
         result = [[1,'canned','no','beans','12oz','red beans','12/25/2022',0,'SanJuan',3],[2,'fruit','yes','organic bannanas','5 lb','bannana','12/25/2022', 4.99,'Ponce',6],[3,'meat','yes','codero flesh','8 Oz','Corderito','12/25/2022',0,'Aguadilla',1]]
         return result
